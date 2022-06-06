0e0984cd50836b9d79b20e682c18cbb796cf5496
Tom MAUGER-BIROCHEAU <92787752+TomMB45@users.noreply.github.com>
2022-06-06 11:38:47 +0200

    Bug fix on the DpSet function

--- superexacttestpy/tl/basic.py
+++ superexacttestpy/tl/basic.py
@@ -104,15 +104,21 @@ def DpSets(x,data,n,logP=False):
     l_data = len_data(data)
     mini = min(l_data) 
     nL = len(data) # len of the dataset 
-    if mini < 0 or any(l_data)>n or any (l_data)<x  : 
+    for nb in l_data : 
+        if nb > n or nb < x : 
+            if not logP : 
+                return 0 
+            print("Invalid input")
+            return False 
+    if mini < 0 :
         if not logP : 
             return 0 
-        print("invalid input")
+        print("Invalid input")
         return False 
     
     if nL < 2 :
         print("data should have at least 2 entries")
-        return  False 
+        return False 
     
     res = dmvHyper2(x=x,nL=len(data),L=len_data(data),n=n,p=0.0,logp=logP)
     return res
