ac679a4ebd5572acf1fddb394153991422c4f6f1
Tom MAUGER-BIROCHEAU <92787752+TomMB45@users.noreply.github.com>
2022-06-07 11:57:34 +0200

    Add the mset function

--- superexacttestpy/tl/__init__.py
+++ superexacttestpy/tl/__init__.py
@@ -1,1 +1,1 @@
-from .basic import basic_tool,len_data,logChoose,dmvHyper,dhyper,DpSets,intersect,dmvHyper_logVal,cpsets_sim,cpsets
+from .basic import basic_tool,len_data,logChoose,dmvHyper,dhyper,DpSets,intersect,dmvHyper_logVal,cpsets_sim,cpsets,mset
