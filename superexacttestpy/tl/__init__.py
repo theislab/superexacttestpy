f48719bd5d5c622f1c32f67ea5cda926851acf3c
Tom MAUGER-BIROCHEAU <92787752+TomMB45@users.noreply.github.com>
2022-06-08 20:57:07 +0200

    Add the function in the init file

--- superexacttestpy/tl/__init__.py
+++ superexacttestpy/tl/__init__.py
@@ -1,1 +1,21 @@
-from .basic import basic_tool,len_data,logChoose,dmvHyper,dhyper,DpSets,intersect,dmvHyper_logVal,cpsets_sim,cpsets,mset
+from .basic import (
+    basic_tool,
+    len_data,
+    logChoose,
+    dmvHyper,
+    dhyper,
+    DpSets,
+    intersect,
+    dmvHyper_logVal,
+    cpsets_sim,
+    cpsets,
+    mset,
+    mkBarcode_degree,
+    IncIntersect,
+    intersectElements,
+    exclusiveIntersect0,
+    exc2incIntersect,
+    enumerateIntersectSizes,
+    formating,
+    supertest
+    )
